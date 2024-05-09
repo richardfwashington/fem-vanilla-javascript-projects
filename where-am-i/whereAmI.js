aafe8d89b38331d2f64ecf793f97b28475dbf77c
Richard Washington <richard.washington@taxassist.co.uk>
2024-05-09 18:33:10 +0100

    🚸  Add a nice cow

--- where-am-i/whereAmI.js
+++ where-am-i/whereAmI.js
@@ -1,4 +1,7 @@
+import { say } from 'cowsay';
+
 console.log('You are here');
 const currentDirectory = process.cwd();
 setTimeout(() => console.log(currentDirectory), 1000);
 
+console.log(say({text: 'Yes I really mean it'}));
