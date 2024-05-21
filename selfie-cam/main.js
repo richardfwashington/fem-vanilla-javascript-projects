a144b93a5b737c4c3e4677643010f9c2d793f046
Richard Washington <richard.washington@taxassist.co.uk>
2024-05-21 16:36:21 +0100

    :sparkles: Build selfie cam

--- selfie-cam/main.js
+++ selfie-cam/main.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { getVideo, drawVideo } from "./src/camera";
 
-const video = await getVideo();
-const canvas = document.querySelector("canvas");
-const button = document.querySelector("#take");
+(async function run() {
+  const video = await getVideo();
+  const canvas = document.querySelector("canvas");
+  const button = document.querySelector("#take");
 
-button.addEventListener("click", () => {
-  console.log("Taking a picture!");
-  drawVideo(video, canvas);
-});
+  button.addEventListener("click", () => {
+    console.log("Taking a picture!");
+    drawVideo(video, canvas);
+  });
+})();
