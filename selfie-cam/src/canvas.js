70a93971ac11af3e0e12ade731656d2bac46832a
Richard Washington <richard.washington@taxassist.co.uk>
2024-05-21 15:23:27 +0100

    :construction: Add new file for the camera canvas

--- selfie-cam/src/canvas.js
+++ selfie-cam/src/canvas.js
@@ -1,8 +1,8 @@
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
-context.fillStyle = 'yellow';
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+context.fillStyle = "yellow";
 context.fillRect(0, 0, 100, 100);
 
-context.fillStyle = 'black';
+context.fillStyle = "black";
 context.font = "50px sans-serif";
-context.fillText('JS!', 40, 90, 50);
+context.fillText("JS!", 40, 90, 50);
